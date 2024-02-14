93010233614fe3418f0f770fa49de5b7b08f524a
pavlav <pa.lavrentyev@gmail.com>
2024-02-15 00:19:52 +0300

    Add comments commit

--- app/src/main/java/com/lavrent/coursescompose/MainActivity.kt
+++ app/src/main/java/com/lavrent/coursescompose/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -26,6 +29,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.lavrent.coursescompose.data.DataSource
 import com.lavrent.coursescompose.model.Topic
 import com.lavrent.coursescompose.ui.theme.CoursesComposeTheme
 
@@ -39,7 +43,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Greeting()
+                    GreetingPreview() // Вызывю тут функцию предпоказа
                 }
             }
         }
@@ -47,7 +51,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TopicCard(topic: Topic, modifier: Modifier = Modifier) { // Карточка
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) { // Карточка, описание одной карточки
     Card { // Элемент карточка
         Row {  // Ряд
             Box { // Элемент контейнер
@@ -72,17 +76,17 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) { // Карточка
                     )
                 )
                 Row(verticalAlignment = Alignment.CenterVertically) { // Ряд, с вертикальным выравниванием по середине
-                    Icon(painter = painterResource(
-                        id = R.drawable.ic_grain),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .padding(start = dimensionResource(id = R.dimen.padding_medium))
+                    Icon(painter = painterResource( // Иконка
+                        id = R.drawable.ic_grain), // Ресурс иконки
+                        contentDescription = null, // Описание иконки
+                        modifier = Modifier // Модификатор
+                            .padding(start = dimensionResource(id = R.dimen.padding_medium)) // отступ в начале
                     )
-                    Text(
-                        text = topic.availableCourses.toString(),
-                        style = MaterialTheme.typography.labelMedium,
-                        modifier = Modifier
-                            .padding(start = dimensionResource(id = R.dimen.padding_small))
+                    Text( // Текст
+                        text = topic.availableCourses.toString(), // Текст взятый изз модели класса данных
+                        style = MaterialTheme.typography.labelMedium, // Стиль
+                        modifier = Modifier //Модификатор
+                            .padding(start = dimensionResource(id = R.dimen.padding_small)) // Отступ в начале
                     )
                 }
             }
@@ -90,10 +94,27 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) { // Карточка
     }
 }
 
+@Composable
+fun TopicGrid(modifier: Modifier = Modifier) { // Сетка из тем
+    LazyVerticalGrid( // Прокручиваемая вертикальная сетка, и его свойсва в конструкторе
+        columns = GridCells.Fixed(2), // Функция задает определенное колличество столбцов в сетке, тут 2
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small)), // Выравнивание каждой карточки относительно друг друга по вертикали
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small)), // Выравнивание каждой карточки относительно друг друга по горизонтали
+        modifier = modifier // Модификатор указал потому что он указан в конструкторе фукнции, чтоб использовался
+    ) { // Сценарий в лямбда функции
+        items(DataSource.topics) { // Добавляет ряд элементов, из дата класса
+            topic -> TopicCard(topic = topic)  // каждый элемент сетки добаляется в карточку, и показывается визуализируется
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview() { // Функ. предпоказа
     CoursesComposeTheme {
-        TopicCard(topic = Topic(R.string.photography, 321, R.drawable.photography))
+        TopicGrid(  // функция сетки тем
+            modifier = Modifier // Модификатор
+                .padding(dimensionResource(id = R.dimen.padding_small)) // отступ со всех сторон, каждой карточки в сетке
+        )
     }
 }
